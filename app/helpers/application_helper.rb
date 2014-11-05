e25003fb5c36be36a9de51efe77c231d08b61619
Aleksey Gureiev <spyromus@noizeramp.com>
2014-11-05 20:12:28 +0300

    styling head submenu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def submenu_item(code, label, path)
+    link = content_tag(:a, label, href: path)
+    content_tag('li', link, class: @submenu == code ? 'active' : nil)
+  end
+
 end
