9cf055798b19e12ac408084fb21906cde5cb158d
Aleksey Gureiev <spyromus@noizeramp.com>
2015-04-01 22:16:18 +0300

    added tests for creating and joining projects

--- test/tasks/seed_test_data_test.rb
+++ test/tasks/seed_test_data_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class SeedTestDataTest < ActiveSupport::TestCase
 
   setup do
-    VCR.use_cassette "seeding" do
+    VCR.use_cassette "seeding/setup" do
       load_user 'admin_user'
       @res = SeedTestData.new(@username, @password).perform
     end
   end
 
   test 'users should be added' do
-    VCR.use_cassette "seeding-testing-users" do
+    VCR.use_cassette "seeding/testing-users" do
       user = @res[:user_ids].first
       login = user[1]
       pass = user[0].split(' ').first
@@ -18,8 +18,26 @@ class SeedTestDataTest < ActiveSupport::TestCase
     end
   end
 
-  # test 'user should own a project'
-  # test 'user should be added to a project'
+  test 'user should own a project' do
+    VCR.use_cassette "seeding/owning-project" do
+      user_id = @res[:user_ids]['Abigail Adams']
+      assert DeterLab.valid_credentials?(user_id, 'Abigail')
+
+      project_ids = DeterLab.view_projects(user_id).map(&:project_id)
+      assert project_ids.include?('Alfa-Romeo')
+    end
+  end
+
+  test 'user should be added to a project' do
+    VCR.use_cassette "seeding/joining-project" do
+      user_id = @res[:user_ids]['Abigail Adams']
+      assert DeterLab.valid_credentials?(user_id, 'Abigail')
+
+      project = DeterLab.view_projects(user_id).select { |p| p.project_id == 'Beta-Test' }.first
+      assert_not_nil project
+      assert_not_equal user_id, project.owner
+    end
+  end
 
   private
 
