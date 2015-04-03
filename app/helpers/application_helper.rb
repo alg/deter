8319bb6451052911fb17c78f9bd9d38af45ea38c
Aleksey Gureiev <spyromus@noizeramp.com>
2015-04-03 17:02:59 +0300

    factored out submenu dropdowns and added experiments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     content_tag('li', link, class: @submenu == code ? 'active' : nil)
   end
 
+  def dropdown_submenu_item(codes, label, &block)
+    items = capture(&block)
+    li_content = [
+      content_tag(:a, label, href: '#', class: "dropdown-toggle", data: { toggle: 'dropdown' }, role: 'button', 'aria-expanded' => 'false'),
+      content_tag(:ul, items, class: 'dropdown-menu', role: 'menu')
+    ].join.html_safe
+
+    content_tag(:li, li_content, class: codes.include?(@submenu) ? 'active' : nil)
+  end
+
 end
