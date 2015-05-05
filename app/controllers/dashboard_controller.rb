5bcbdd77bd4ca67c9d37ada2c9c07b0007b3f02b
Aleksey Gureiev <spyromus@noizeramp.com>
2015-05-05 13:21:00 +0300

    listing notifications

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,9 @@ class DashboardController < ApplicationController
 
   before_filter :require_login
 
+  # shows user dashboard
   def show
+    @notifications = DeterLab.get_notifications(current_user_id)
   end
 
 end
