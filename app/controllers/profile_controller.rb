86a497a866af28bdc0290fe76ef389ca7ba73395
Aleksey Gureiev <spyromus@noizeramp.com>
2015-05-05 10:19:51 +0300

    redirecting to my profile after successful update

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -23,7 +23,7 @@ class ProfileController < ApplicationController
     @errors = DeterLab.change_user_profile(app_session.current_user_id, form_fields)
     if @errors.blank?
       deter_lab.invalidate_profile
-      redirect_to :profile, notice: t(".success")
+      redirect_to :my_profile, notice: t(".success")
     else
       @profile = deter_lab.get_profile
       flash.now[:alert] = t(".failure")
--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -2,11 +2,21 @@ require 'test_helper'
 
 class ProfileControllerTest < ActionController::TestCase
 
-  test "request is redirected to login when session is lost" do
+  setup do
     AppSession.new(@controller.session).logged_in_as "mark"
+  end
+
+  test "request is redirected to login when session is lost" do
     @controller.deter_lab.expects(:get_profile).raises(DeterLab::NotLoggedIn)
     get :show_my_profile
     assert_redirected_to :login
+  end
+
+  test "redirecting to profile on successful update" do
+    DeterLab.expects(:change_user_profile).returns(nil)
+    @controller.deter_lab.expects(:invalidate_profile)
+    put :update, profile: {}
+    assert_redirected_to :my_profile
   end
 
 end
