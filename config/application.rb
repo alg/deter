3ad5684dc0de4d8206269f831b70e1574637e424
Aleksey Gureiev <spyromus@noizeramp.com>
2015-05-16 23:27:27 +0300

    different icon appoach

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ module Deter
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.enabled = true
-    config.assets.precompile += %w{ favicon.ico }
 
     config.cache_store = :redis_store, 'redis://localhost:6379/0/deter-cache', { expires_in: 90.minutes }
   end
