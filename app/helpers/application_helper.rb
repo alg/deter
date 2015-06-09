c2b4cdaccbb3f3420583a10eeb9f6a53d2294079
Aleksey Gureiev <spyromus@noizeramp.com>
2015-06-09 23:09:51 +0300

    showing TBD edit buttons for projects and manage buttons for project experiments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,7 +26,7 @@ module ApplicationHelper
   def section_row(title, section_id, state = :closed)
     content_tag(:div, [
       content_tag(:div, content_tag(:h4, title), class: 'col-sm-11'),
-      content_tag(:div, content_tag(:button, state == :opened ? 'Hide' : 'Show', :class => 'btn btn-xs btn-default', :type => 'button', :data => { toggle: 'collapse', target: "##{section_id}" }, "aria-expanded" => false, "aria-controls" => section_id), class: 'col-sm-1')
+      content_tag(:div, content_tag(:button, state == :opened ? 'Hide' : 'Show', :class => 'btn btn-xs btn-default', :type => 'button', :data => { toggle: 'collapse', target: "##{section_id}" }, "aria-expanded" => false, "aria-controls" => section_id), class: 'col-sm-1 text-right')
     ].join.html_safe, class: 'row section-row')
   end
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,23 @@ class Project < Struct.new(:project_id, :owner, :approved, :members)
     self.project_id.try(:downcase) == 'admin'
   end
 
+  # True if can modify ACL
+  def can_modify_access?(uid)
+    owner == uid || (members.find { |m| m.uid == uid && m.permissions.include?("ADD_USER") && m.permissions.include?("REMOVE_USER") }.present?)
+  end
+
+  # True if the user can modify the project
+  def can_modify?(uid)
+    owner == uid
+  end
+
+  private
+
+  # normalized permissions list which is always an array without blanks
+  def normalized_perms
+    unless defined? @normalized_perms
+      @normalized_perms = [ self.perms ].flatten.reject(&:blank?)
+    end
+    @normalized_perms
+  end
 end
